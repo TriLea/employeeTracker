758b3f200f6275a1db4324eb0dc42a4b2aed2839
trista_lea <tristan_lea@outlook.com>
2023-03-10 21:24:25 -0800

    created queries, updated queries.sql

--- db/schema.sql
+++ db/schema.sql
@@ -19,10 +19,10 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL REFERENCES employee(id), /* self-referencing */
+  manager_id INT REFERENCES employee(id), /* self-referencing */
   FOREIGN KEY (role_id)
   REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,6 @@ VALUES ("Sales Lead", 100000, 1),
        ("Lead Engineer", 120000, 2);
      
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 1),
+VALUES ("John", "Doe", 1, NULL),
        ("Jane", "Doe", 2, 1),
        ("Sally", "Smith", 3, 2);
