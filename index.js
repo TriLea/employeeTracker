dbe74f10caff4ae362430c63f72a4edb9e410a44
trista_lea <tristan_lea@outlook.com>
2023-03-11 18:56:16 -0800

    inquirer basic setup

--- index.js
+++ index.js
@@ -14,60 +14,277 @@ const db = mysql.createConnection(
       database: '' //fix
     },
     console.log(`Connected to the movies_db database.`) // fix
-  );
+);
 
-function questionSet1() //name //function to start the app?
+function menu()
 {
     inquirer.prompt(
-    
+        [
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'choice',
+                choices: ['Add employee', 
+                'Update employee',
+                 'View all roles', 
+                 'Add role', 
+                'View all departments', 
+                'Add department']
+            }
+        ]
+        
+    )
+    .then((choice)=> {
+
+        switch(choice.choices) {
+            case "":
+                addEmployee();
+            break;
+
+            case "":
+                updateEmployee();
+            break;
+
+            case "":
+                viewAllRoles();
+            break;
+
+            case "":
+                addRole();
+            break;
+
+            case "":
+                viewAllDepartments();
+            break;
+
+            case "":
+                addDepartment();
+            break;
+
+            default:
+                console.log("incorrect input");
+        }
+    })
+}
+
+function addEmployee()
+{
+    inquirer.prompt(
         [
             {
                 type: 'input',
-                message: 'What is your name?',
-                name: 'name'
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
             },
             {
                 type: 'input',
-                message: 'What is your ID?',
-                name: 'ID'
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
             },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
         ]
     )
     .then((answers) => {
-        const managerObj = new Manager(answers.name, answers.ID)
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
 
-        const sql = `INSERT INTO movies (movie_name) 
-        VALUES (?)`;
-        const params = [body.movie_name];
-
-        questionSetTwo(); //employee meny would display choices
+        menu(); //employee meny would display choices
         //and direct to the appropriate function
     })
 }
 
-function () //name //function to start the app?
+function updateEmployee()
 {
     inquirer.prompt(
-    
         [
             {
                 type: 'input',
-                message: 'What is your name?',
-                name: 'name'
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
             },
             {
                 type: 'input',
-                message: 'What is your ID?',
-                name: 'ID'
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
             },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
         ]
     )
     .then((answers) => {
-        const managerObj = new Manager(answers.name, answers.ID)
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
 
-        employeeMenu(); //employee meny would display choices
+        menu(); //employee meny would display choices
         //and direct to the appropriate function
     })
 }
 
-//call start
+function viewAllRoles()
+{
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
+        ]
+    )
+    .then((answers) => {
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
+
+        menu(); //employee meny would display choices
+        //and direct to the appropriate function
+    })
+}
+
+function addRole()
+{
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
+        ]
+    )
+    .then((answers) => {
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
+
+        menu(); //employee meny would display choices
+        //and direct to the appropriate function
+    })
+}
+
+function viewAllDepartments()
+{
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
+        ]
+    )
+    .then((answers) => {
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
+
+        menu(); //employee meny would display choices
+        //and direct to the appropriate function
+    })
+}
+
+function addDepartment()
+{
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: 'lastName'
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s role?',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager'
+            }
+        ]
+    )
+    .then((answers) => {
+        //save the data?
+        // const sql = `INSERT INTO movies (movie_name) 
+        // VALUES (?)`;
+        // const params = [body.movie_name];
+
+        menu(); //employee meny would display choices
+        //and direct to the appropriate function
+    })
+}
